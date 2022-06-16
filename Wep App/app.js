f1fb616cc04f00f425d3d9ed53d321ca31222267
Lucas-Terto <68282530+Lucas-Terto@users.noreply.github.com>
2022-06-16 08:42:46 -0300

    Add files via upload

---
+++ Wep App/app.js
@@ -0,0 +1,66 @@
+let hun = 0
+let h = 0
+let min = 0
+let sec = 0
+let time = 10
+let chrono
+
+var cont = 0
+
+function startstop(){
+
+    if(cont == 0){
+        chrono = setInterval(() => {
+            timer()
+        }, time);
+        let stop = "PAUSE"
+        document.getElementById("btn-startstop").innerHTML = stop
+        document.getElementById("btn-startstop").setAttribute("class", "btn-stop")
+        cont++
+    }else{
+        clearInterval(chrono)
+        let start = "CONTINUE"
+        document.getElementById("btn-startstop").innerHTML = start
+        document.getElementById("btn-startstop").setAttribute("class", "btn-continue")
+        cont = 0
+    }
+   
+}
+
+/* function stop(){
+    clearInterval(chrono)
+} */
+
+function reset(){
+    
+    clearInterval(chrono)
+    hun = 0
+    h = 0
+    min = 0
+    sec = 0
+    document.getElementById("counter").innerHTML = "0:00:00.00"
+
+    let start = "START"
+    document.getElementById("btn-startstop").innerHTML = start
+    document.getElementById("btn-startstop").setAttribute("class", "startstop")
+    cont = 0
+   
+}
+
+function timer(){
+    hun++
+    if(hun == 100){
+        hun = 0
+        sec++
+        if(sec == 60){
+            sec = 0
+            min++
+            if(min == 60){
+                min = 0
+                h++
+            }
+        }
+    }
+    let format = (h) + ":" + (min < 10 ? "0" + min : min) + ":" + (sec < 10 ? "0" + sec : sec) + "." + (hun < 10 ? "0" + hun : hun)
+    document.getElementById("counter").innerHTML = format
+}
---
+++ Wep App/timer/script.js
@@ -0,0 +1,111 @@
+let start = document.getElementById("start")
+let reset = document.getElementById("reset")
+
+let h = document.getElementById("hour")
+let m = document.getElementById("minute")
+let s = document.getElementById("sec")
+
+//store a reference to the variable
+let startTimer = null
+let input = document.getElementById("inputs")
+let cont = 0
+
+function startstop(){
+    if(cont == 0){
+        function startInterval(){
+            startTimer = setInterval(function() {
+                timer();
+            }, 1000);
+        }
+        startInterval();
+        let pause = "PAUSE"
+        document.getElementById("start").innerHTML = pause
+        document.getElementById("start").setAttribute("class", "btn-pause")
+        input.setAttribute("class", "hide")
+        cont++
+    }else{
+        clearInterval(startTimer);
+        let start = "CONTINUE"
+        document.getElementById("start").innerHTML = start
+        document.getElementById("start").setAttribute("class", "btn-continue")
+        cont = 0
+    }
+    
+}
+
+function timer(){
+
+    if(h.value > 99){
+        h.value = 99
+    }
+    if(m.value > 60){
+        m.value = 60
+    }
+    if(s.value > 60){
+        s.value = 60
+    }
+    if(h.value < 0){
+        h.value = h.value * -1
+    }
+    if(m.value < 0){
+        m.value = m.value * -1
+    }
+    if(s.value < 0){
+        s.value = s.value * -1
+    }
+
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0
+        m.value = 0
+        s.value = 0
+        clearInterval(startTimer)
+        let format = "00:00:00"
+        document.getElementById("counter").innerHTML = format
+        let start = "START"
+        document.getElementById("start").innerHTML = start
+        document.getElementById("start").setAttribute("class", "btn-start")
+        input.setAttribute("class", "show")
+        cont = 0
+    }else if(s.value != 0){
+        s.value--
+    }else if(m.value != 0 && s.value == 0){
+        s.value = 59
+        m.value--
+    }else if(h.value != 0 && m.value == 0){
+        m.value = 60
+        h.value--
+    }
+
+    let format = (h.value < 10 ? "0" + h.value : h.value) + ":" + (m.value < 10 ? "0" + m.value : m.value) + ":" + (s.value < 10 ? "0" + s.value : s.value)
+    document.getElementById("counter").innerHTML = format
+
+    /* return */
+
+}
+
+reset.addEventListener("click", function(){
+    h.value = 0
+    m.value = 0
+    s.value = 0
+    clearInterval(startTimer)
+
+    let format = "00:00:00"
+    document.getElementById("counter").innerHTML = format
+    let start = "START"
+    document.getElementById("start").innerHTML = start
+    document.getElementById("start").setAttribute("class", "btn-start")
+    input.setAttribute("class", "show")
+    cont = 0
+})
+
+
+/* start.addEventListener("click", function(){
+    //initialize the variable startTimer
+    function startInterval(){
+        startTimer = setInterval(function() {
+            timer()
+        }, 1000)
+    }
+    startInterval()
+}) */
+
